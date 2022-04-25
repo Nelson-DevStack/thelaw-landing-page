105197a7ab7bdbd568477924d9b263e6e6a99c00
Nelson C <n.devpro@protonmail.com>
2022-04-24 20:57:39 -0400

    dev1.0

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -11,6 +11,11 @@ export default function Hero(){
             <p className={style.text}>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam impedit voluptatem sint nisi aliquam harum quo eos quia? Numquam, inventore.
             </p>
+            <div className={style.contentFooter}>
+              <button className={style.contentButton}>
+                Who we are
+              </button>
+            </div>
           </div>
         </div>
       </div>
