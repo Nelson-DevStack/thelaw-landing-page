8b65c11feb9f2e54b7d7807cb1659acd463feda9
Nelson C <n.devpro@protonmail.com>
2022-04-26 21:14:09 -0400

    dev1.3

---
+++ components/Footer/index.tsx
@@ -0,0 +1,50 @@
+import { FaFacebook, FaFacebookF, FaLinkedin, FaLinkedinIn, FaTwitter } from 'react-icons/fa';
+import style from './Footer.module.css';
+
+export default function Footer(){
+  return(
+    <footer className={style.footer}>
+      <div className={style.container}>
+
+        <div className={style.first}>
+          <div className={style.iconsArea}>
+            <div className={style.iconWrapper}>
+              <FaFacebookF />
+            </div>
+            <div className={style.iconWrapper}>
+              <FaTwitter />
+            </div>
+            <div className={style.iconWrapper}>
+              <FaLinkedinIn />
+            </div>
+          </div>
+
+          <div className={style.footerForm}>
+            <input className={style.formInput} type="text" />
+            <button className={style.formBtn}>
+              Lorem ipsum
+            </button>
+          </div>
+
+        </div>
+
+        <div className={style.footerInfo}>
+          <p>Copyright The Law, 2022</p>
+          <ul className={style.footerLinks}>
+            <li>
+              Licenses
+            </li>
+            <li>
+              Styles
+            </li>
+            <li>
+              About
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+    </footer>
+  )
+}
