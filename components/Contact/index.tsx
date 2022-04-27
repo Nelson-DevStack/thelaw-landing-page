2f47883a32bec8146e63b4ac1f6e1b13750caa46
Nelson C <n.devpro@protonmail.com>
2022-04-27 14:44:09 -0400

    dev1.6

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -54,7 +54,9 @@ export default function Contact(){
             </div>
 
             <div className={style.mapArea}>
-              Maps
+              {/* Maps */}
+              {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.612966503309!2d-73.98795218452885!3d40.74854111534378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9aeb1c6b5%3A0x35b1cfbc89a6097f!2sEmpire%20State%20Building%2C%20New%20York%2C%20NY%2010001%2C%20USA!5e0!3m2!1sen!2sbr!4v1651084495500!5m2!1sen!2sbr" width="400" height="300" style={{border: "0"}}allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> */}
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.612966503309!2d-73.98795218452885!3d40.74854111534378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9aeb1c6b5%3A0x35b1cfbc89a6097f!2sEmpire%20State%20Building%2C%20New%20York%2C%20NY%2010001%2C%20USA!5e0!3m2!1sen!2sbr!4v1651084495500!5m2!1sen!2sbr"  style={{border: '0'}} allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
           </div>
